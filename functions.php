b5203d8bfa3cbe8b0c088695c278d52b52613e31
Jahangir <designermja@gmail.com>
2021-09-06 19:54:41 +0600

    linking css js and bootstrap

--- functions.php
+++ functions.php
@@ -5,3 +5,23 @@
  * @package Aquila
  */
 add_theme_support('title-tag');
+function aquila_enqueue_scripts(){
+//    register styles
+    wp_register_style("bootstrap-css", get_template_directory_uri().'/assets/libraries/bootstrap/css/bootstrap.min.css', array(), false, 'all');
+    wp_register_style('stylesheet', get_stylesheet_uri(),array(),filemtime(get_stylesheet_directory().'/style.css'),'all');
+    wp_register_style("another-style", get_template_directory_uri().'/assets/css/another.css', array(), filemtime(get_template_directory().'/assets/css/another.css'), 'all');
+
+//    Register scripts
+    wp_register_script("bootstrap-js", get_template_directory_uri().'/assets/libraries/bootstrap/js/bootstrap.bungle.min.js', array(), false, true);
+    wp_register_script("main-js", get_template_directory_uri().'/assets/js/main.js', array(), filemtime(get_template_directory().'/assets/js/main.js'), true);
+
+//    enqueue styles
+    wp_enqueue_style("bootstrap-css");
+    wp_enqueue_style("stylesheet");
+    wp_enqueue_style("another-style");
+
+//    enqueue scripts
+    wp_enqueue_script("bootstrap-js");
+    wp_enqueue_script("main-js");
+}
+add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
--- header.php
+++ header.php
@@ -14,5 +14,10 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
+<?php
+if (function_exists('wp_body_open')){
+    wp_body_open();
+}
+
+?>
 <header>Header</header>
